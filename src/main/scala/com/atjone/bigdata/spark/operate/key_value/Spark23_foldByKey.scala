5e108d69b4f6a5234446bcadc54ad3a0ac7f9acd
jone <jone@163.com>
2020-05-29 17:36:00 +0800

    spark

--- src/main/scala/com/atjone/bigdata/spark/operate/key_value/Spark23_foldByKey.scala
+++ src/main/scala/com/atjone/bigdata/spark/operate/key_value/Spark23_foldByKey.scala
@@ -18,9 +18,9 @@ object Spark23_foldByKey {
       * foldByKey是aggregateByKey的一种简化，分区内函数和分区间函数一样，所以可以省略一个函数
       */
 
-    //foldByKey(0)(_ + _)也可以写成rdd.aggregateByKey(0)(_ + _, _ + _)
     val foldRDD: RDD[(Int, Int)] = rdd.foldByKey(0)(_ + _)
     val aggRDD: RDD[(Int, Int)] = rdd.aggregateByKey(0)(_ + _, _ + _)
+    //不能进行类型推断，所以不能写成(_ + _, _ + _)
     val combineRDD: RDD[(Int, Int)] = rdd.combineByKey(x => x, (x: Int, y: Int) => (x + y), (x: Int, y: Int) => (x + y))
     combineRDD.collect().foreach(println)
   }
